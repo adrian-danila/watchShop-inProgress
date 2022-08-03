d765cbcd23ec23f39a7bc34ac156277cef55e0f4
Adrian Danila <adrian.danila22@gmail.com>
2022-08-03 18:32:02 +0300

    I created a functions that make Sign In button disabled when the input it's empty

---
+++ scripts/login.js
@@ -0,0 +1,10 @@
+
+document.getElementById("submit").disabled = true;
+
+document.getElementById("email").addEventListener("keyup", e => {
+    if(e.target.value ==""){
+        document.getElementById("submit").disabled = true;
+    } else {
+        document.getElementById('submit').disabled = false;
+    }
+});
--- scripts/sign-up.js
+++ scripts/sign-up.js
@@ -1,3 +1,5 @@
+// Trying to implement functions for password - min 8 carachters, min 1 special carachter
+
 const form = document.querySelector("form");
 form.addEventListener("submit", function(event) {
     event.preventDefault();
@@ -8,5 +10,7 @@ const [username, password, confirm-password] = document.querySelectorAll('input'
 password.addEventListener('blur',function(){
     if(password.length === 0) {
         password.style.border = '20px solid red';
+    } else if () {
+
     }
 })
