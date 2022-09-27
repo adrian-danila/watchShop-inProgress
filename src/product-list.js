0112ffa9a10b4a07c18f4673cc40b5c72deffcd3
Adrian Danila <adrian.danila22@gmail.com>
2022-09-27 11:08:27 +0300

    adde pagination on product list

--- src/product-list.js
+++ src/product-list.js
@@ -227,3 +227,20 @@ sortingSelection.addEventListener('change', (e) => {
 
 //Products Pagination
 
+const itemPerPage = 8;
+let currentPage = 1;
+
+generateProducts([...products].splice(0,itemPerPage));
+
+// console.log(products);   // make a copy with [...] to keep all products 
+
+const nextButton = document.getElementById('next_button');
+const prevButton = document.getElementById('prev_button');
+
+nextButton.addEventListener('click', () => {
+    const currentIndex = currentPage *itemPerPage
+    const productFP = [...products].splice(currentIndex, itemPerPage);
+    generateProducts(productFP);
+    currentPage++;
+    
+});
