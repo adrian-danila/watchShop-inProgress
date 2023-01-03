c140fc5511dd1520f85162c045bf17c2db1083f9
Adrian Danila <adrian.danila22@gmail.com>
2023-01-03 19:04:19 +0200

    new design for login and sign-up page is ready

--- src/sign-up-old.js
+++ src/sign-up-old.js
@@ -1,3 +1,23 @@
+// Trying to implement functions for password - min 8 carachters, min 1 special carachter
+
+// const form = document.querySelector("form");
+// form.addEventListener("submit", function(event) {
+//     event.preventDefault();
+// });
+
+// const [username, pwd] = document.querySelectorAll('input');
+
+// password.addEventListener('blur',function(){
+//     if(pwd.length === 0) {
+//         pwd.style.border = '20px solid red';
+//     } else if () {
+
+//     }
+// })
+
+
+
+//All inputs required
 
 
 
