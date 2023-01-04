7bf653ce6dca2b871c7259780b6195dd07e8afe0
Adrian Danila <adrian.danila22@gmail.com>
2023-01-04 17:20:46 +0200

    added a new design for table on product details

--- src/product-list-pagination.js
+++ src/product-list-pagination.js
@@ -1,14 +1,15 @@
 // Products Pagination
 
-const itemPerPage = 5;
+const itemPerPage = 10;
 let currentPage = 1;
 
-generateProducts([...products].splice(0,itemPerPage));
+// generateProducts([...products].splice(0,itemPerPage));
 
 // console.log(products);   // make a copy with [...] to keep all products 
 
-const nextButton = document.getElementById('next_button');
 const prevButton = document.getElementById('prev_button');
+const nextButton = document.getElementById('next_button');
+
 
 nextButton.addEventListener('click', () => {
     const currentIndex = currentPage * itemPerPage
