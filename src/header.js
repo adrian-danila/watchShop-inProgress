2e90bea106c46c5aae0a17b11778cc6ed2640c5f
Adrian Danila <adrian.danila22@gmail.com>
2023-01-06 11:20:10 +0200

    big updates for this project, i created nav,header css files and make responsive header and nav

--- src/header.js
+++ src/header.js
@@ -1,3 +1,5 @@
+const hamburgerIcon = document.querySelector('header ul li:last-of-type i');
+
 function updateShoppingCartQuantity() {
     const shoppingCartQuantitySpan = document.querySelector(".shopping-cart-quantity");
     shoppingCartQuantitySpan.textContent = getTotalQuantityLS();
@@ -6,3 +8,12 @@ function updateShoppingCartQuantity() {
     // Sa-ti iei span-ul cu document.queryselector dupa clasa
     // iti creezi o functie in localstoarge.js care-ti returneaza cantiatea totala numita getTotalQuantityLS
 }
+
+hamburgerIcon.addEventListener('click', function () {
+    if (nav.classList.contains('show')) {
+        nav.classList.remove('show');
+    }
+    else {
+        nav.classList.add('show');
+    }
+});
--- src/sign-up.js
+++ src/sign-up.js
@@ -1,37 +1,40 @@
-
-
-
-// Login and Local Storage accounts
+const showPasswordIcon = document.querySelector('.show-password-icon');
+const passwordInput = document.querySelector('#password');
 
 const signUp = e => {
-    let fname = document.getElementById('fname').value,
-        lname = document.getElementById('lname').value,
-        email = document.getElementById('email').value,
-        pwd = document.getElementById('pwd').value;
+    const firstName = document.getElementById('firstName').value;
+    const lastName = document.getElementById('lastName').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
 
     let formData = JSON.parse(localStorage.getItem('formData')) || [];
 
-    let exist = formData.length && 
-        JSON.parse(localStorage.getItem('formData')).some(data => 
-            data.fname.toLowerCase() == fname.toLowerCase() && 
+    let exist = formData.length &&
+        JSON.parse(localStorage.getItem('formData')).some(data =>
+            data.fname.toLowerCase() == fname.toLowerCase() &&
             data.lname.toLowerCase() == lname.toLowerCase()
         );
 
-    if(!exist){
-        formData.push({ fname, lname, email, pwd });
+    if (!exist) {
+        formData.push({ fname, lname, email, password });
         localStorage.setItem('formData', JSON.stringify(formData));
         document.querySelector('form').reset();
         document.getElementById('fname').focus();
         alert("Account Created.\n\nPlease Sign In using the link below.");
     }
-    else{
+    else {
         alert("Ooopppssss... Duplicate found!!!\nYou have already signed up");
     }
     e.preventDefault();
 }
 
-
-//Password length
-
-function passwordLength
-
+showPasswordIcon.addEventListener('click', function () {
+    if(passwordInput.type === 'password')
+    {
+        passwordInput.type = 'text';
+    }
+    else
+    {
+        passwordInput.type = 'password';
+    }
+});
