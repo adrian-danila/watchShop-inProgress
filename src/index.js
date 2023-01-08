7494c8c03d4adb602584564ae2fa4d3464015c58
Adrian Danila <adrian.danila22@gmail.com>
2023-01-08 18:43:33 +0200

    added minor changes and deleted unnecessary files

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ searchInput.addEventListener('keydown',function(event){
     if (event.keyCode === 13) {
         window.location.href = `${window.location.origin}/products-list.html?searchTerm=${event.target.value}`;
     }
-    // console.log(event);
 });
 
 searchButton.addEventListener('click', function(){
